417394d03c0b670c8902ae99f674ececb6f57688
anhkhoaAK47 <khoaanh4567@gmail.com>
2025-11-22 17:15:41 +0700

    login_page and signup_page

--- myProject/BackEnd/server.js
+++ myProject/BackEnd/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '@Heo0938757774',
+    password: 'Bebovien@123',
     database: 'recipetine'
 });
 
